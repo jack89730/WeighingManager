a21056b56b00710352d5b98ab4a21c5f7dc0575d
zcs2631 <zcs2631@126.com>
2015-04-23 15:49:12 +0800

    Revert "modify"
    This reverts commit 8095272c429be2850a06b69f224a8034d3d105ab.

--- poundscontrolApi/poundscontrolprotocol.h
+++
@@ -1,9 +0,0 @@
-#ifndef __POUNDSCONTROLPROTOCOL_H__
-#define __POUNDSCONTROLPROTOCOL_H__
-
-// 读取仪表状态信息:寄存器起始地址=0x21,寄存器数量=1
-// 读取仪表显示重量值:寄存器起始地址=0x06,寄存器数量=4
-int readstatus(unsigned char * data, int len);
-int readweight(unsigned char * data, int len);
-
-#endif
