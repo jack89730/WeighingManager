3b66e6459ef95beba4c33d5783219b8541257ac0
zhangys529 <zhangyingshuo1988@qq.com>
2015-05-11 18:08:59 +0800

    ledcontrlolApi改成了ledcontrolApi，增加

---
+++ WeighingManager/WeighingManagerImp.cpp
@@ -0,0 +1,7 @@
+#include "WeighingManagerImp.h"
+#include "stdafx.h"
+
+int CWeighingManagerImp::process_start()
+{
+	return 0;
+}
---
+++ WeighingManager/WeighingManagerImp.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// 此类为业务实现类，实现称重管理系统的自动化功能
+class CWeighingManagerImp
+{
+public:
+	CWeighingManagerImp();
+	~CWeighingManagerImp();
+
+public:
+	int process_start();
+
+};
